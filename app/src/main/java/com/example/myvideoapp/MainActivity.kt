9ff74395423ce7c9f052394e3f99b415472cdaea
rchampa <ricardo.champa@gmail.com>
2019-04-25 00:40:41 +0200

    uploading more videos

--- app/src/main/java/com/example/myvideoapp/MainActivity.kt
+++ app/src/main/java/com/example/myvideoapp/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.myvideoapp
 
+import android.content.res.Configuration
+import android.content.res.Configuration.*
 import android.net.Uri
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -25,12 +28,26 @@ class MainActivity : AppCompatActivity() {
             videoBackground.setVideoURI(getUri())
             true
         }
+
+        checkSize()
     }
 
     fun getUri() : Uri {
 
         val videoPath = "android.resource://" + applicationContext.packageName + "/"
-        val uri = Uri.parse(videoPath + R.raw.background)
+        val uri = Uri.parse(videoPath + R.raw.background)//not working :(
+//        val uri = Uri.parse(videoPath + R.raw.background_no_audio)//working!!! (no audio signal)
         return uri
+    }
+
+    fun checkSize(){
+        val screenSize = resources.configuration.screenLayout and SCREENLAYOUT_SIZE_MASK
+        when(screenSize){
+            SCREENLAYOUT_SIZE_SMALL -> { Log.d("MEHHHH","small") }
+            SCREENLAYOUT_SIZE_NORMAL -> { Log.d("MEHHHH","normal") }
+            SCREENLAYOUT_SIZE_LARGE -> { Log.d("MEHHHH","large") }
+            SCREENLAYOUT_SIZE_XLARGE -> { Log.d("MEHHHH","xlarge") }
+            SCREENLAYOUT_SIZE_UNDEFINED -> { Log.d("MEHHHH","undefined") }
+        }
     }
 }
